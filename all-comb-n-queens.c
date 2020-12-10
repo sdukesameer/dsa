7e4bb46a6674cb58c3919259d8592948301a613e
SAMEER <sdukesameer@gmail.com>
2020-12-11 02:47:12 +0530

    "night 11-12-2020"

--- all-comb-n-queens.c
+++ all-comb-n-queens.c
@@ -1,57 +1,8 @@
 #include<stdio.h>
 int N;//no of row col
-int safe(int arr[N][N], int row, int col){//to check if the column is safe
-    int i,j;
-    for ( i = 0; i < col; i++)//if column is filled return 0
-        if(arr[row][i])
-            return 0;
-    for ( i = row, j = col; i >= 0 && j >= 0; i--, j--)//if upper diagonal is filled return 0
-        if(arr[i][j])
-            return 0;
-    for ( i = row, j = col; i < N && j >= 0; i++, j--)//if lower diagonal is filled return 0
-        if(arr[i][j])
-            return 0;
-    return 1;//else id safe 
-}
-int queens(int arr[N][N],int n){//n is col no, initially its 0
-    if(n>=N)//best case, when all queens are well placed
-        return 1;
-    for (int i = 0; i < N; i++)//surf accross rows
-    {
-        if(safe(arr,i,n)){
-            arr[i][n]=1;//if safe insert a queen
-            if(queens(arr,n+1))//then check if at next col its safe to place queen
-                return 1; 
-            arr[i][n]=0;//if on next col queens can't be placed backtrack and remove queen from that location
-        }
-    }
-    return 0;//when solution doesn't exist
-}
-int main(){
-    int n=1;
-    while(n){//check for invalid input
-        printf("Enter valid N i.e no of rows and columns: ");
-        scanf("%d",&N);
-        if(N>0)
-            n=0;
-    }
-    int arr[N][N];
-    for (int i = 0; i < N; i++)//initiaise all to 0
-        for (int j = 0; j < N; j++)
-            arr[i][j]=0;
-    int ans=queens(arr,0);//queen function call
-    if(ans==0)
-        printf("\nFor N = %d, solution doesn't exist.\n",N);
-    if(N==2){
-        printf("Maximum 1 queen can be placed on board.\n");
-        arr[0][0]=1;
-    }
-    if(N==3){
-        printf("Maximum 2 queens can be placed on board.\n");
-        arr[0][0]=1;
-        arr[2][1]=1;
-    }
-    printf("\n");//graphical representation
+void display(int arr[N][N]){//graphical representation
+    static int n=1;
+    printf("\nSolution No. %d: \n",n++);
     for (int i = 0; i < N; i++){
         if(i==0){
             for (int k = 0; k < N; k++)
@@ -70,5 +21,48 @@ int main(){
             printf(" ---");
         printf("\n");
     }
+}
+int safe(int arr[N][N], int row, int col){//to check if the column is safe
+    int i,j;
+    for ( i = 0; i < col; i++)//if column is filled return 0
+        if(arr[row][i])
+            return 0;
+    for ( i = row, j = col; i >= 0 && j >= 0; i--, j--)//if upper diagonal is filled return 0
+        if(arr[i][j])
+            return 0;
+    for ( i = row, j = col; i < N && j >= 0; i++, j--)//if lower diagonal is filled return 0
+        if(arr[i][j])
+            return 0;
+    return 1;//else id safe 
+}
+void queens(int arr[N][N],int n){//n is col no, initially its 0
+    if(n>=N){//best case, when all queens are well placed
+        display(arr);
+        return;
+    }
+    for (int i = 0; i < N; i++){//surf accross rows
+        if(safe(arr,i,n)){
+            arr[i][n]=1;//if safe insert a queen
+            queens(arr,n+1); //then check if at next col its safe to place queen
+            arr[i][n]=0;//if on next col queens can't be placed backtrack and remove queen from that location
+        }
+    }
+}
+int main(){
+    int n=1;
+    while(n){//check for invalid input
+        printf("Enter valid N i.e no of rows and columns: ");
+        scanf("%d",&N);
+        if(N>0)
+            n=0;
+    }
+    int arr[N][N];
+    for (int i = 0; i < N; i++)//initiaise all to 0
+        for (int j = 0; j < N; j++)
+            arr[i][j]=0;
+    if(N==2 || N==3)
+        printf("\nFor N = %d, solution doesn't exist.\n",N);
+    else
+        queens(arr,0);//queen function call
     return 0;
 }//end of main function
--- all-comb-n-rooks.c
+++ all-comb-n-rooks.c
@@ -12,7 +12,7 @@ void display(int arr[N][N]){//graphical representation
         printf("|");
         for (int j = 0; j < N; j++){
             if(arr[i][j])
-                printf(" B |");
+                printf(" R |");
             else
                 printf("   |");
         }
@@ -23,31 +23,23 @@ void display(int arr[N][N]){//graphical representation
     }
 }
 int safe(int arr[N][N], int row, int col){//to check if the column is safe
-    int i,j;
-    for (i = row-1, j = col-1; i >= 0 && j >= 0; i--, j--)//if left upper diagonal is filled return 0
-        if(arr[i][j])
+    for (int i = 0; i < row; i++)//if row is filled return 0
+        if(arr[row][i])
             return 0;
-    for (i = row+1, j = col+1; i < N && j < N; i++, j++)//if right lower diagonal is filled return 0
-        if(arr[i][j])
-            return 0;
-    for (i = row+1, j = col-1; i < N && j >= 0; i++, j--)//if left lower diagonal is filled return 0
-        if(arr[i][j])
-            return 0;
-    for (i = row-1, j = col+1; i >= 0 && j < N; i--, j++)//if right upper diagonal is filled return 0
-        if(arr[i][j])
+    for (int i = 0; i < col; i++)//if col is filled return 0
+        if(arr[row][i])
             return 0;
     return 1;//else id safe 
 }
-void bishops(int arr[N][N], int n){
-    if(n>=N){
+void rooks(int arr[N][N], int n){//n is col no, initially its 0
+    if(n>=N){//best case, when all rooks are well placed
         display(arr);
         return;
     }
-    for (int i = 0; i < N; i++)
-    {
+    for (int i = 0; i < N; i++){//surf accross rows
         if(safe(arr,i,n)){
             arr[i][n]=1;//if safe insert a rook
-            bishops(arr,n+1);//then check if at next col its safe to place rook
+            rooks(arr,n+1);//then check if at next col its safe to place rook
             arr[i][n]=0;//if on next col rooks can't be placed backtrack and remove rook from that location
         }
     }
@@ -55,15 +47,15 @@ void bishops(int arr[N][N], int n){
 int main(){
     int n=1;
     while(n){//check for invalid input
-        printf("Enter valid N i.e. no of rows and columns: ");
+        printf("Enter valid N i.e no of rows and columns: ");
         scanf("%d",&N);
         if(N>0)
             n=0;
     }
     int arr[N][N];
-    for (int i = 0; i < N; i++)//initialise all to 0
+    for (int i = 0; i < N; i++)//initiaise all to 0
         for (int j = 0; j < N; j++)
             arr[i][j]=0;
-    bishops(arr,0);//bishops function call
+    rooks(arr,0);//rooks function call
     return 0;
 }//end of main function
